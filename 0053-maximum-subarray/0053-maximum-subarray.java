6bdc03a6ee0a15912536251c95b823061f10206d
Shadab Alam <shadab29012005@gmail.com>
2026-01-25 18:12:35 +0530

    Time: 1 ms (99.6%), Space: 77.3 MB (39.84%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,25 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max =Integer.MIN_VALUE;
-        for (int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+= nums[j];
-                max = Math.max(max,sum);
-            }
+
+            // Kadan's Algo
+        int sum =0;
+        int max=Integer.MIN_VALUE;
+        for(int i = 0; i<nums.length;i++){
+            sum += nums[i];
+            max= Math.max(max,sum);
+            if(sum<0)sum=0;
         }
         return max;
+
+        // int max =Integer.MIN_VALUE;
+        // for (int i=0;i<nums.length;i++){
+        //     int sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         sum+= nums[j];
+        //         max = Math.max(max,sum);
+        //        if(sum>max)max=sum;
+        //    }
+        //}
+        //return max;
     }
 }
