7fbeece48dfe910eff4f553ad5149c1b8600e98c
Shadab Alam <shadab29012005@gmail.com>
2026-02-03 19:53:07 +0530

    Time: 5 ms (0.61%), Space: 44.9 MB (14.29%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,10 @@
+class Solution {
+  public int maximumWealth(int[][] accounts) {
+    return Arrays.stream(accounts)
+        .mapToInt(account -> Arrays.stream(account).sum())
+        .max()
+        .getAsInt();
+  }
+}
+
+
