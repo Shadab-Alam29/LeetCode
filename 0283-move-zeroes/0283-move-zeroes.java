52ada73d1c1540d495ef5fef3f5f57ad96d7ea6b
Shadab Alam <shadab29012005@gmail.com>
2026-02-03 19:29:59 +0530

    Time: 3 ms (19.62%), Space: 48.2 MB (15.01%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int j=0;
+        int i;
+        int n=arr.length;
+
+
+        for(i=0;i<n;i++){
+            if(arr[i]!=0&& arr[j]==0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+            if(arr[j]!=0){
+                j++;
+            }
+        }
+    }
+}
+  /*      for (;j<nums.length;j++){
+            if(nums[j]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+            }
+            
+        }
+    }
+}*/
