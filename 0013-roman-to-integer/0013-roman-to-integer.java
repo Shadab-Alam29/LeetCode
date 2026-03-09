aaff1cdd438a86186584b8dac2513dc34be9911d
Shadab Alam <shadab29012005@gmail.com>
2026-03-10 02:33:45 +0530

    Time: 5 ms (35.78%), Space: 47.1 MB (9.97%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,41 +1,23 @@
- class Solution {
+class Solution {
     public int romanToInt(String s) {
-	        int arr[]=new int[s.length()];
-	       for(int i=0;i<s.length();i++) {
-	    	   switch (s.charAt(i)) {
-	            case 'I':
-	                arr[i]= 1;
-	                break;
-	            case 'V':
-	            	arr[i]= 5;
-	            	break;
-	            case 'X':
-	            	arr[i]= 10;
-	            	break;
-	            case 'L':
-	            	arr[i]= 50;
-	            	break;
-	            case 'C':
-	            	arr[i]= 100;
-	            	break;
-	            case 'D':
-	            	arr[i]= 500;
-	            	break;
-	            case 'M':
-	            	arr[i]= 1000;
-	            	break;
-	       }
-	       }
-	     int num=0;
-	       for(int i=0;i<s.length()-1;i++) {
-	    	     if(arr[i]>=arr[i+1]) {
-	    		 num=arr[i]+num;
-	    	   }
-	    	   else {
-	    		   num=num+(arr[i+1]-arr[i]);
-	    		  i=i+1;
-	    	   }
-	       }
-	        return num;
+        int res = 0;
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
+                res -= roman.get(s.charAt(i));
+            } else {
+                res += roman.get(s.charAt(i));
+            }
         }
+
+        return res + roman.get(s.charAt(s.length() - 1));        
+    }
 }
