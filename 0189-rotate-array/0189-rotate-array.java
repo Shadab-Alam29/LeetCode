f432ba34bce097ee36ab3db19969d4f4995fac48
Shadab Alam <shadab29012005@gmail.com>
2026-03-10 06:41:03 +0530

    Time: 2 ms (24.35%), Space: 63.3 MB (12.45%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int []arr = new int [nums.length];
+        for(int i =0 ;i< nums.length;i++){
+            int index = (i+k)% nums.length;
+            arr[index]=nums[i];
+        }
+        for(int i = 0; i < nums.length ;i++){
+            nums[i]=arr[i];
+        }
+    }
+}
