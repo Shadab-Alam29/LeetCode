937ccfb35cda6e75342dd75f733aeb123e69e5c6
Shadab Alam <shadab29012005@gmail.com>
2026-03-12 00:42:58 +0530

    Time: 433 ms (5.05%), Space: 42.1 MB (90.41%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -1,12 +1,18 @@
 class Solution {
   public int countPrimeSetBits(int left, int right) {
-    final int m= 665772;
-    int ans = 0;
-
-    for (int num = left; num <= right; ++num)
-      if ((m >> Integer.bitCount(num) & 1) == 1)
-        ++ans;
-
-    return ans;
+    int count = 0;
+    for (int i = left ; i<=right;i++){
+        if( hammingWeight(i) == 2 || hammingWeight(i) == 3 ||hammingWeight(i) == 5 ||hammingWeight(i) == 7 ||hammingWeight(i) == 11 ||hammingWeight(i) == 13 ||hammingWeight(i) == 17 ||hammingWeight(i) == 19 )
+        count ++;
+    }
+    return count ;
   }
+   public int hammingWeight(int n) {
+        int c = 0;
+        while(n>= 1){
+            if(n%2 == 1 )c++;
+            n = n/2;
+        }
+        return c ;
+    }
 }
