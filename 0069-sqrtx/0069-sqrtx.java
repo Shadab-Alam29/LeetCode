463f6e7209c568a7ab4479634d1c5650c2b2f263
Shadab Alam <shadab29012005@gmail.com>
2026-03-16 23:29:35 +0530

    Time: 33 ms (5.01%), Space: 42.9 MB (19.29%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,15 +1,20 @@
 class Solution {
   public int mySqrt(long x) {
-    long l = 1;
-    long r = x + 1;
 
-    while (l < r) {
-      final long m = (l + r) / 2;
-      if (m > x / m)
-        r = m;
-      else
-        l = m + 1;
+    if (x < 2) {
+        return (int)x;
     }
-    return (int) l - 1;
+    long ans = x/2;
+    for (long i = 0; i <= x / 2; i++) {
+        if (i * i == x) {
+            ans = (int)i;
+            break;
+        }
+        if (i * i > x) {
+            ans= (int)(i - 1);
+            break;
+        }
+    }
+return (int)ans ;
   }
 }
