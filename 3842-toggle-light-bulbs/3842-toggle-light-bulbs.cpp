0028bec0729122ddc6bee9f3d53eacf4899a782b
Shadab Alam <shadab29012005@gmail.com>
2026-03-17 00:53:53 +0530

    Time: 0 ms (100%), Space: 33.2 MB (87.8%) - LeetHub

---
+++ 3842-toggle-light-bulbs/3842-toggle-light-bulbs.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> toggleLightBulbs(vector<int>& bulbs) {
+        vector<int> vec;
+
+        sort(bulbs.begin(), bulbs.end());
+
+        int count = 1;
+
+        for(int i = 1; i < bulbs.size(); i++){
+            if(bulbs[i] == bulbs[i-1]){
+                count++;
+            } 
+            else{
+                if(count % 2 != 0){
+                    vec.push_back(bulbs[i-1]);
+                }
+                count = 1;
+            }
+        }
+
+        if(count % 2 != 0){
+            vec.push_back(bulbs[bulbs.size()-1]);
+        }
+
+        return vec;
+    }
+};
