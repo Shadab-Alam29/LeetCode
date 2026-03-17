959ce7a0130de6b5cc9503e499315fbb65cbcf90
Shadab Alam <shadab29012005@gmail.com>
2026-03-17 17:53:21 +0530

    Time: 6 ms (46.99%), Space: 42 MB (82.38%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        if(n==2){
+            return 1;
+        }
+        return fib(n-2)+fib(n-1);
+    }
+}
