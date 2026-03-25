e057f64d231840781d4d796dbc0f5e669df1607a
Shadab Alam <shadab29012005@gmail.com>
2026-03-25 10:18:10 +0530

    Time: 5 ms (16.13%), Space: 43.5 MB (22.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution { 
+    public boolean isValid(String s) {
+    Stack<Character> st = new Stack<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (st.isEmpty()) {
+                st.push(ch);
+            } 
+            else if ((st.peek() == '[' && ch == ']') || 
+                     (st.peek() == '{' && ch == '}') || 
+                     (st.peek() == '(' && ch == ')')) {
+                st.pop();
+            } 
+            else {
+                st.push(ch);
+            }
+        } 
+        return st.isEmpty();
+    }
+}
