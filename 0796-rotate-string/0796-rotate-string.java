5304eb2a9a31b718a82ed9c70a3006b248d6577c
Shadab Alam <shadab29012005@gmail.com>
2026-03-30 09:57:30 +0530

    Time: 1 ms (24.4%), Space: 43 MB (35.81%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+       for(int i = 0 ;i <= s.length();i++){
+        String rot = s.substring(i)+s.substring(0,i);
+        if(rot.equals (goal))return true ;
+       } 
+       return false ;
+    }
+}
