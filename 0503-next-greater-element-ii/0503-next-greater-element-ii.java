afd43548855e524d3b49577657c0a01d9d650cdb
Shadab Alam <shadab29012005@gmail.com>
2026-03-30 09:36:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,17 +1,19 @@
 class Solution {
      public int[] nextGreaterElements(int[] nums) {
         int max = nums[0];
-         int i = 0;
-          for(; i< nums.length-1;i++)
-          { if(nums[i]<nums[i+1] )
-          { nums[i] = nums[i+1];
-           max = nums[i+1]% nums.length;
-            }
-             else nums[i] = -1 ;
-              }
-               for(;i<nums.length;i++)
-               { nums[i]= max ;
-                } 
-                return nums;
-                 }
-                  }
+        int i = 0;
+        int ans [] = new int [nums.length];
+        Arrays.fill(ans,-1);
+        for(; i< nums.length-1;i++){
+        if(nums[i]<nums[i+1] ){
+            ans[i] = nums[i+1];
+            max = nums[i+1]% nums.length;
+        }
+        else ans[i] = -1 ;
+    }
+        for(;i<nums.length;i++){
+             ans[i]= max ;
+        } 
+        return ans;
+    }
+}
