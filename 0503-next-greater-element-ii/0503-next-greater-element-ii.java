ed9b0d12806db9bf37ec69231e27db257c192c00
Shadab Alam <shadab29012005@gmail.com>
2026-03-30 09:44:15 +0530

    Time: 14 ms (59.05%), Space: 48.3 MB (71.74%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,19 +1,25 @@
 class Solution {
-     public int[] nextGreaterElements(int[] nums) {
-        int max = nums[0];
-        int i = 0;
-        int ans [] = new int [nums.length];
+    public int[] nextGreaterElements(int[] nums) {
+        int ans[] = new int[nums.length];
         Arrays.fill(ans,-1);
-        for(; i< nums.length-1;i++){
-        if(nums[i]<nums[i+1] ){
-            ans[i] = nums[i+1];
-            max = nums[i+1]% nums.length;
+        /*for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                int idx = (i+j)%nums.length;
+                if(nums[idx]>nums[i]){
+                    ans[i]=nums[idx];
+                    break;
+                }
+            }
+        }*/
+        Stack<Integer> s = new Stack<>();
+        for(int i=0;i<2*nums.length;i++){
+            int idx = i%nums.length;
+            while(!s.isEmpty() && nums[idx]>nums[s.peek()]){
+                int in = s.pop();
+                ans[in] = nums[idx];
+            }
+            s.push(idx);
         }
-        else ans[i] = -1 ;
-    }
-        for(;i<nums.length;i++){
-             ans[i]= max ;
-        } 
         return ans;
     }
 }
