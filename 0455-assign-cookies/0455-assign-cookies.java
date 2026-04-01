efa3fd2dbc74dd2d30789049d44e161996365696
Shadab Alam <shadab29012005@gmail.com>
2026-04-01 11:52:00 +0530

    Time: 15 ms (64.5%), Space: 51.4 MB (81.57%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -3,15 +3,27 @@ class Solution {
      Arrays.sort(g);
      Arrays.sort(s);
      int count = 0;
-     for(int i = 0 ; i< g.length;i++){
-        for(int j = 0 ; j<s.length; j++){
-            if(s[j]>=g[i]){
-                count ++ ;
-                s[j] = -1;
-                break;
-            }
+    int i = 0 ,j = 0 ;
+    while(i<g.length && j<s.length){
+        if(s[j]>=g[i]){
+            count++;
+            i++;
+            j++;
         }
-     }
+        else{
+            j++;
+        }
+    }
+
+    //  for(int i = 0 ; i< g.length;i++){
+    //     for(int j = 0 ; j<s.length; j++){
+    //         if(s[j]>=g[i]){
+    //             count ++ ;
+    //             s[j] = -1;
+    //             break;
+    //         }
+    //     }
+    //  }
      return count ;   
     }
 }
