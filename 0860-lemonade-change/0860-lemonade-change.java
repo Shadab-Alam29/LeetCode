4a7050f0c0df18b4e41b4ec2cb79789f55fcdf46
Shadab Alam <shadab29012005@gmail.com>
2026-04-13 08:35:54 +0530

    Time: 2 ms (99.7%), Space: 72.5 MB (55.15%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int f = 0 , t = 0 ;
+         for(int b : bills){
+            if (b == 5){
+                f++;
+            }
+            else if (b == 10 ){
+                if ( f >= 1){
+                f --;
+                t ++ ;
+                }
+                else return false ;
+            }
+            else {
+                if (t >0 && f>0){
+                    t--;
+                    f--;
+                }
+                else if (f>=3){
+                    f-=3;
+                }
+                else return false ;
+            }
+         }
+         return true ;
+    }
+}
