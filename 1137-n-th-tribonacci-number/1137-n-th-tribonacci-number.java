5d2140ea7a8aa57152ae1809238c8ed04c901554
Shadab Alam <shadab29012005@gmail.com>
2026-04-22 11:03:17 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.03%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,18 +1,15 @@
 class Solution {
     public int tribonacci(int n) {  
-      int dp = [] new int [ n+1 ];
-      tri( n ,dp);
-
+      int dp[] = new int [ n+1 ];
+      return tri(n,dp);
     }
         public int tri(int n , int dp []) {  
         if ( n == 0 ) return 0;
         if ( n == 1 || n == 2 ) return 1 ;
-        if ( dp[n]!=0)return dp[n];
-        return tri( n-1)+ tri(n-2) + tri(n-3);
+        if ( dp[n]!=0) return dp[n];
+        return dp[n]= tri( n-1,dp)+ tri(n-2,dp) + tri(n-3,dp);
 
     }
-
-
 }
 
 
