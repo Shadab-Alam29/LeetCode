241b5df3b4babc49240db49e8a7bbec348a7d12d
Shadab Alam <shadab29012005@gmail.com>
2026-06-08 13:29:51 +0530

    Time: 9 ms (42.54%), Space: 42.1 MB (32.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,13 @@
 class Solution {
     public int fib(int n) {
 
-        return f(n);
-    }
-    int f( int n){
+    //    return f(n);
+
+  //  int f( int n){
         if ( n<=1)return n;
-        return f(n-1)+f(n-2);
+        return fib(n-1)+fib(n-2);
     }
+   // }
             
         // int a = 0 , b = 1 ;
         // for ( int i = 0 ; i<n ;i++){
