8df3ad689e1c6c6db4acd50582a887e1b531a611
Shadab Alam <shadab29012005@gmail.com>
2026-06-09 10:16:12 +0530

    Time: 36 ms (6.14%), Space: 98.2 MB (27.48%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer>nums = new ArrayList<>();
+        for ( int i = 1 ; i<= n ;i++)nums.add(i);
+        List<List<Integer>>ans = new ArrayList<>();
+        fun(nums,ans,new ArrayList<>(),k,0);
+        return ans;
+    }
+    void fun(List<Integer> nums,List<List<Integer>> ans, List<Integer> c, int k , int idx){
+        if ( c.size()==k){
+        List<Integer>copy= new ArrayList<>();
+        for ( int i : c)copy.add(i);
+        ans.add(copy);
+        //ans.add(new ArrayLis<>(C));
+        return;
+        }
+        for(int i = idx; i<nums.size();i++){
+        c.add(nums.get(i));
+        fun(nums,ans,c,k,i+1);
+        c.remove(c.size()-1);
+
+        }
+    }
+}
