85c7df52ec667d63119147a66dfc473443a7c52a
Shadab Alam <shadab29012005@gmail.com>
2026-06-13 22:37:49 +0530

    Time: 9 ms (93.99%), Space: 73.8 MB (5.98%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,11 +1,34 @@
 class Solution {
     public int totalFruit(int[] fruits) {
+        
+//         int i = -1 ;
+//         int j = -1 ;
+//         int max = -1;
+//         int arr[]=new int [2];
+//         for ( int k : fruits ){
+//             if ( i==k )
+//                 arr[0]++;
+//             else if ( j==k )
+//                 arr[1]++;
+//             else{
+//                 arr[0]=arr[1];
+//                 i=j;
+//                 j=k;
+//                 arr[1]=1;
+//             }
+//             max = Math.max(max,arr[0]+arr[1]);
+//         }
+//         return max;
+//     }
+// wrong tc for 64
+// }
+
         int fr[] = new int[100001];
         int type = 0;
         int max = 0;
-        int s =0;
-        for(int e=0;e<fruits.length;e++){
-            int f = fruits[e];
+        int s=0;
+        for(int i=0;i<fruits.length;i++){
+            int f = fruits[i];
             if(fr[f]==0)type++;
             fr[f]++;
             while(type>2){
@@ -14,10 +37,9 @@ class Solution {
                 if(fr[sf]==0)type--;
                 s++;
             }
-            int pick = e-s+1;
+            int pick = i-s+1;
             max = Math.max(max,pick);
         }
         return max;
-
     }
 }
