3bee767ee3defd4c55cb0b27e3e971fc24513485
Shadab Alam <shadab29012005@gmail.com>
2026-06-14 23:51:24 +0530

    Time: 0 ms (100%), Space: 43.3 MB (17.1%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,23 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s=s.trim();
-      String words[] =s.split(" ");
-      return words[words.length-1].length();  
+        int f = 0;
+        int c = 0 ;
+        for( int i = s.length()-1; i>=0 ; i--){
+            if (s.charAt(i) == ' '){
+                if ( f == 0 ) continue ;
+                else break;
+            }
+            else {
+                c++;
+                f=1;
+            }
+        }
+        return c ;
+    // s = s.trim();
+    // //String words[] =s.split(" ");
+    // //return words[words.length-1].length();  
+
+    // int len =s.lastIndexOf(" ")+1;
+    // return s.length()-len;
     }
 }
