8e5bcc6f84295aeaee1ad0c9ac87d90d0a1a1925
Shadab Alam <shadab29012005@gmail.com>
2026-06-15 00:31:56 +0530

    Time: 0 ms (100%), Space: 42.3 MB (6.95%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -2,8 +2,7 @@ class Solution {
     public int xorOperation(int n, int start) {
         int ans = 0 ;
         for ( int i =0 ; i<n ; i++ ){
-            int num=start + 2 *i;
-                ans = ans^num;
+               ans = ans^(start + 2 *i);    
         }
         return ans;
     }
