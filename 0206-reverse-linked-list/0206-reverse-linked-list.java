2507f809bd74023ee9bf3120dd6142fa10cbd77e
Shadab Alam <shadab29012005@gmail.com>
2026-06-16 10:26:20 +0530

    Time: 0 ms (100%), Space: 44.3 MB (49.04%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,19 +10,51 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode temp = head ;
-        List<Integer> l = new ArrayList<>();
-        while ( temp != null){
-            l.add(temp.val);
-            temp=temp.next;
+
+        ListNode prev = null ; 
+        ListNode current = head ;
+        while ( current != null){
+            ListNode next_one = current.next;
+            current.next = prev;
+            prev= current ;
+            current = next_one; 
         }
-        int n = l.size()-1;
-        ListNode tem= head;
-        while (tem != null){
-            tem.val = l.get(n);
-            tem=tem.next;
-            n--;
-        }
-        return head ;
+        return prev ;
     }
 }
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ 
+ // cheatcode approch 
+
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         ListNode temp = head ;
+//         List<Integer> l = new ArrayList<>();
+//         while ( temp != null){
+//             l.add(temp.val);
+//             temp=temp.next;
+//         }
+//         int n = l.size()-1;
+//         ListNode tem= head;
+//         while (tem != null){
+//             tem.val = l.get(n);
+//             tem=tem.next;
+//             n--;
+//         }
+//         return head ;
+//     }
+// }
+
