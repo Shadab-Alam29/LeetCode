158b4c95d9324bd92a2acc3670844f4cd17b94f8
Shadab Alam <shadab29012005@gmail.com>
2026-06-17 22:15:11 +0530

    Time: 2 ms (59.25%), Space: 43.9 MB (29.75%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -5,15 +5,11 @@ class Solution {
         if (k == 0) return ans;
         for (int i = 0; i < n; i++) {
             int sum = 0;
-            if (k > 0) {
-                for (int j = 1; j <= k; j++) {
-                    sum += code[(i + j) % n];
-                }
-            } else {
-                for (int j = 1; j <= -k; j++) {
-                    sum += code[(i - j + n) % n];
-                }
-            }
+            if (k > 0) 
+                for (int j = 1; j <= k; j++)sum += code[(i + j) % n];
+            else 
+                for (int j = 1; j <= -k; j++)sum += code[(i - j + n) % n];   
+
             ans[i] = sum;
         }
         return ans;
