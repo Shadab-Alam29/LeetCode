a9cc89340a656d458a6978f8483f4cbcd21ae2d9
Shadab Alam <shadab29012005@gmail.com>
2026-06-18 16:25:51 +0530

    Time: 2 ms (21.62%), Space: 46.4 MB (96.18%) - LeetHub

--- 3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.java
+++ 3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.java
@@ -13,10 +13,8 @@ class Solution {
         }
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            boolean greaterThanLeft =
-                (i == 0) || (nums[i] > prefMax[i - 1]);
-            boolean greaterThanRight =
-                (i == n - 1) || (nums[i] > sufMax[i + 1]);
+            boolean greaterThanLeft = (i == 0) || (nums[i] > prefMax[i - 1]);
+            boolean greaterThanRight = (i == n - 1) || (nums[i] > sufMax[i + 1]);
             if (greaterThanLeft || greaterThanRight) {
                 ans.add(nums[i]);
             }
