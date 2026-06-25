5c80f3e25cb31f90eec7f8d6ee20bd6e21be1663
Shadab Alam <shadab29012005@gmail.com>
2026-06-25 12:56:38 +0530

    Time: 0 ms (100%), Space: 43.4 MB (83.58%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,29 +15,45 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<List<Integer>> ans = levelOrder(root);
-        List<Integer> res = new ArrayList<>();
-        for ( int i = 0 ;i < ans.size();i++){
-            res.add(ans.get(i).get(ans.get(i).size() - 1));
-        }
-        return res ;
+       List<Integer> l = new ArrayList<>();
+       fun(root , l , 0);
+       return l; 
     }
-     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        if (root == null)return ans;
-        q.add(root);
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> l = new ArrayList<>();
-            while (size-- > 0) {
-                TreeNode f = q.poll();
-                l.add(f.val);
-                if (f.left != null) q.add(f.left);
-                if (f.right != null) q.add(f.right);
-            }
-            ans.add(l);
-        }
-        return ans;
+    void fun ( TreeNode root , List<Integer> l , int level ){
+        if ( root == null) return ;
+        if (level == l.size())l.add(root.val);
+        fun(root.right , l , level+1);
+        fun(root.left , l , level+1);  
     }
 }
+
+// using level order traversing 
+
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<List<Integer>> ans = levelOrder(root);
+//         List<Integer> res = new ArrayList<>();
+//         for ( int i = 0 ;i < ans.size();i++){
+//             res.add(ans.get(i).get(ans.get(i).size() - 1));
+//         }
+//         return res ;
+//     }
+//      public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         Queue<TreeNode> q = new LinkedList<>();
+//         if (root == null)return ans;
+//         q.add(root);
+//         while (!q.isEmpty()) {
+//             int size = q.size();
+//             List<Integer> l = new ArrayList<>();
+//             while (size-- > 0) {
+//                 TreeNode f = q.poll();
+//                 l.add(f.val);
+//                 if (f.left != null) q.add(f.left);
+//                 if (f.right != null) q.add(f.right);
+//             }
+//             ans.add(l);
+//         }
+//         return ans;
+//     }
+// }
