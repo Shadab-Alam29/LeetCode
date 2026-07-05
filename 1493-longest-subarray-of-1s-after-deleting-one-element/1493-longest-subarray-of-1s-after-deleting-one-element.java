85fc2f945948db5289499645ebe8fbee076c9c90
Shadab Alam <shadab29012005@gmail.com>
2026-07-05 22:45:38 +0530

    Time: 3 ms (88.04%), Space: 65.3 MB (84.69%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,19 +1,37 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-    int max =0 ;
-    int start = 0;
-    int fr []= new int[2];
-    for(int e = 0 ; e < nums.length ; e++ ){
-        int value = nums[e];
-        fr[value]++;
+        int maxm = 0;
+        int n = nums.length;
+        int idx = -1;
+        int sum = 0;
         
-        while(fr[0]>1){
-            int sch = nums[start++];
-            fr[sch]--;
-        }
-    int size = e-start;
-    max = Math.max(max,size);
-    }
-    return max;
+        for(int i = 0; i <= n - 1; i++){
+            if(nums[i] == 0)
+            {
+                int prev_ones = i - 1 - idx;
+                idx = i;
+                sum = prev_ones;
+            }
+            else sum = sum + nums[i];
+            maxm = Math.max(maxm, sum);
+        }     
+        if(maxm == n) return maxm - 1;
+        return maxm;
+
+    // int max =0 ;
+    // int start = 0;
+    // int fr []= new int[2];
+    // for(int e = 0 ; e < nums.length ; e++ ){
+    //     int value = nums[e];
+    //     fr[value]++;
+        
+    //     while(fr[0]>1){
+    //         int sch = nums[start++];
+    //         fr[sch]--;
+    //     }
+    // int size = e-start;
+    // max = Math.max(max,size);
+    // }
+    // return max;
     }
 }
