d1d52d34ad8966b72e5e9b9533b447bf7d1bdb19
Shadab Alam <shadab29012005@gmail.com>
2026-07-05 17:09:07 +0530

    Time: 2593 ms (5.37%), Space: 69.4 MB (88.58%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,22 +1,32 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        Queue<Integer> q = new LinkedList<>();
-        int sum = 0;
-        int maxSum = Integer.MIN_VALUE;
-
-        for (int num : nums) {
-            q.offer(num);
-            sum = sum + num;
-
-            if (q.size() > k) {
-                sum -= q.poll();
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;        
+        for ( int i = 0 ;i<=n-k ; i++){
+            int sum = 0 ;
+            for ( int j = i ; j<i+k ; j++){
+                sum = sum +nums[j];
             }
-
-            if (q.size() == k) {
-                maxSum = Math.max(maxSum, sum);
-            }
+            max = Math.max(max,sum) ;
         }
+        
+        return (double) max / k;
 
-        return (double) maxSum / k;
+
+        // Queue<Integer> q = new LinkedList<>();
+        // int sum = 0;
+        // int maxSum = Integer.MIN_VALUE;
+        // for (int num : nums) {
+        //     q.offer(num);
+        //     sum = sum + num;
+        //     if (q.size() > k) {
+        //         sum -= q.poll();
+        //     }
+        //     if (q.size() == k) {
+        //         maxSum = Math.max(maxSum, sum);
+        //     }
+        // }
+        // return (double) maxSum / k;
+
     }
 }
