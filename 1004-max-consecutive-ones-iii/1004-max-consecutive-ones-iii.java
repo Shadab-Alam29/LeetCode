b08c15d4b00caa2cf4bd889de44c2a574ecac03c
Shadab Alam <shadab29012005@gmail.com>
2026-07-05 17:43:41 +0530

    Time: 5 ms (11.67%), Space: 57.4 MB (6.49%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-      int max =0 ;
+    int max =0 ;
     int start = 0;
     int fr []= new int[k+2];
     for(int e = 0 ; e < nums.length ; e++ ){
@@ -15,5 +15,17 @@ class Solution {
     max = Math.max(max,size);
     }
     return max;  
+    //   int zero=0 , s=0 , max=0;
+    //       for( int e=0 ; e<nums.length;e++){
+    //         if(nums[e]==0) zero++;
+            
+    //         while(zero>k){
+    //             if(nums[s]==0) zero--;
+    //             s++;
+    //         }
+    //         int size=e-s+1;
+    //         max=Math.max(max,size);
+    //       }
+    //       return max;
     }
-}
+    }
