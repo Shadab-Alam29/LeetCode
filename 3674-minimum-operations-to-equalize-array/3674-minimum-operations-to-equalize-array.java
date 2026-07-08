021fb8ff70bd4a8ba38fb3b1b224aebef739ed3f
Shadab Alam <shadab29012005@gmail.com>
2026-07-08 23:21:03 +0530

    Time: 0 ms (100%), Space: 45 MB (67.76%) - LeetHub

--- 3674-minimum-operations-to-equalize-array/3674-minimum-operations-to-equalize-array.java
+++ 3674-minimum-operations-to-equalize-array/3674-minimum-operations-to-equalize-array.java
@@ -2,7 +2,7 @@ class Solution {
     public int minOperations(int[] nums) {
         for (int num : nums) {
             if (num != nums[0]) {
-                return 1;
+                return 1 ;
             }
         }
         return 0;
