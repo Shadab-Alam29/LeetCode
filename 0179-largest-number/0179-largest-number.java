1ced1fe3db56682f84c80be8dd5464ed7f617e2d
Shadab Alam <shadab29012005@gmail.com>
2026-07-14 22:16:17 +0530

    Time: 6 ms (96.33%), Space: 45.1 MB (45.05%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -3,7 +3,7 @@ class Solution {
         String ans = "";
         String[] arr = new String[nums.length];
         for(int i = 0; i < nums.length; i++){
-            arr[i] = nums[i] + "";
+            arr[i] = nums[i] + "";  // make it as a string 
         }
         Arrays.sort(arr, (a,b) -> (b+a).compareTo(a+b));
         if(arr[0].equals("0"))  return "0";
