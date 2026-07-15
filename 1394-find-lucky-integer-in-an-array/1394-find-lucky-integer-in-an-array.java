fb99a0c7d4588817ca04910579c74cda1cb30dbc
Shadab Alam <shadab29012005@gmail.com>
2026-07-15 21:54:20 +0530

    Time: 1 ms (100%), Space: 45.2 MB (36.11%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
@@ -1,17 +1,32 @@
 class Solution {
     public int findLucky(int[] arr) {
-        int max = -1 ;
-        HashMap<Integer, Integer> freq = new HashMap<>();
-            for ( int i : arr){
-                if ( i <= 500 && freq.containsKey(i))
-                     freq.put(i, freq.get(i) + 1);
-                else    freq.put(i, 1); 
-            }
 
-            for ( int key : freq.keySet()){
-                 if (key == freq.get(key)) 
-                     max = Math.max(max, key);
+        int max = -1 ;
+        int[] freq = new int[501];
+        for ( int i : arr){
+            if ( i <=500){
+                freq[i]++;
             }
-        return max ;
+        }
+        for ( int i = 1 ; i < freq.length;i++){
+            if ( i == freq[i]){
+                max = Math.max(max, i);
+            }
+        }
+    return max ;
+
+        // int max = -1 ;
+        // HashMap<Integer, Integer> freq = new HashMap<>();
+        //     for ( int i : arr){
+        //         if ( i <= 500 && freq.containsKey(i))
+        //              freq.put(i, freq.get(i) + 1);
+        //         else    freq.put(i, 1); 
+        //     }
+
+        //     for ( int key : freq.keySet()){
+        //          if (key == freq.get(key)) 
+        //              max = Math.max(max, key);
+        //     }
+        // return max ;
     }
 }
