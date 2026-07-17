54e331edf229a47bc5ed59e51207a9cbbad5f95a
Shadab Alam <shadab29012005@gmail.com>
2026-07-17 22:59:17 +0530

    Time: 1 ms (81.01%), Space: 42 MB (99.14%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,5 +1,12 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        return n ;
+        int a = n*n; // oddsum
+        int b = a+n; // evensum
+         while ( b!=0){
+                int temp = b ; 
+                b = a % b ;
+                a = temp ;
+         }
+         return a ;
     }
 }
