77e59bfbdd9004ecfef6cf595d46c0cde0ae0f21
Shadab Alam <shadab29012005@gmail.com>
2026-07-18 20:51:25 +0530

    Time: 21 ms (23.61%), Space: 47.2 MB (69.74%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+       for ( int i = 0 ; i < heights.length ;i++){
+            for ( int j = i+1 ; j < heights.length ; j++){
+                 if(heights[i]<heights[j]){
+                    int temp=heights[i];
+                    String s=names[i];
+                    heights[i]=heights[j];
+                    heights[j]=temp;
+
+                    names[i]=names[j];
+                    names[j]=s;
+                 }
+            }
+         }
+         return names ;
+    }
+}
