5b43f52b6aaab2537780f09bb8e8fe558b767238
Shadab Alam <shadab29012005@gmail.com>
2026-07-18 21:25:53 +0530

    Time: 0 ms (100%), Space: 50.1 MB (67.36%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int ans = 0;
+
+        for(int i=citations.length-1;i>=0;i--){
+            if(citations[i]>=ans+1)
+                ans++;
+            else
+                break;
+        }
+
+        return ans;
+    }
+}
