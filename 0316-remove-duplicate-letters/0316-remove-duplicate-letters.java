fffeb134833dd2feb1f8b6a7429283e8b0fcd3b7
Shadab Alam <shadab29012005@gmail.com>
2026-07-19 23:04:02 +0530

    Time: 2 ms (86.89%), Space: 43.2 MB (95.75%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] ind = new int[26];
+        int[] check = new int[26];
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            ind[s.charAt(i) - 'a'] = i;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (check[c - 'a'] == 1)
+                continue;
+            while (ans.length() > 0 && ans.charAt(ans.length() - 1) > c && ind[ans.charAt(ans.length() - 1) - 'a'] > i) {
+                check[ans.charAt(ans.length() - 1) - 'a'] = 0;
+                ans.deleteCharAt(ans.length() - 1);
+            }
+            ans.append(c);
+            check[c - 'a'] = 1;
+        }
+
+        return ans.toString();
+    }
+}
