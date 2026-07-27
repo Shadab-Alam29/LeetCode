9c9c923a56279f4abb167204ecc8114faf6c789d
Shadab Alam <shadab29012005@gmail.com>
2026-07-27 14:48:38 +0530

    Time: 38 ms (63.79%), Space: 72.9 MB (23.52%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,29 +1,21 @@
 class Solution {
     public boolean canPartition(int[] arr) {
         int sum = 0;
-        for( int i: arr){
-            sum +=i;
-        }
+        for (int num : arr)   sum += num;
         if (sum % 2 != 0) return false;
-        sum = sum / 2;
-        int[][] dp = new int[arr.length][sum + 1];
-        for (int i = 0; i < arr.length; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-
-        return f(arr, arr.length - 1, sum, dp);
+        int target = sum / 2;
+        int[][] dp = new int[arr.length][target + 1];
+        for (int[] row : dp)  Arrays.fill(row, -1);
+        return f(arr, arr.length - 1, target, dp);
     }
 
-    static Boolean f(int[] arr, int index, int target, int[][] dp) {
+    static boolean f(int[] arr, int index, int target, int[][] dp) {
         if (target == 0) return true;
         if (index < 0) return false;
-        if (dp[index][target] != -1)
-        return dp[index][target] == 1;
-        boolean notTake = f(arr, index - 1, target, dp);
-        boolean take = false;
-        if (arr[index] <= target)
-            take = f(arr, index - 1, target - arr[index], dp);
-        boolean ans = take || notTake;
+        if (dp[index][target] != -1)    return dp[index][target] == 1;
+        boolean ans = f(arr, index - 1, target, dp);
+        if (arr[index] <= target) 
+        ans = ans || f(arr, index - 1, target - arr[index], dp);
         dp[index][target] = ans ? 1 : 0;
         return ans;
     }
