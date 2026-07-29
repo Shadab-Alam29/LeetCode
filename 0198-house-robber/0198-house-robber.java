d6c0910da44fbd108afc6850773b2baedc7fe91d
Shadab Alam <shadab29012005@gmail.com>
2026-07-29 19:22:25 +0530

    Time: 0 ms (100%), Space: 42.6 MB (88.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,29 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
-        int dp [] = new int [nums.length];
-        Arrays.fill(dp,-1);
-        return fun (nums,0,dp);
-
-    //     int amount [] = new int [nums.length];
-    //     if (nums.length == 0) return 0;
-    //     if (nums.length == 1) return nums[0];
-    //   //  if(nums.length == 2 ) return Math.max(nums[0],nums[1]);
-    //      amount[0] = nums[0];
-    //      amount[1] = Math.max(nums[0],nums[1]); // nums[1];
-    //     int max_amount = Math.max(nums[0],nums[1]);
-    //     for(int i = 2 ; i <=nums.length-1;i++){
-    //         amount[i] = Math.max(amount[i-1], nums[i] + amount[i-2]);
-    //         max_amount = Math.max(amount[i],amount[i-1]);
-    //     }
-    //     return max_amount ; 
-
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        int []dp = new int [n];
+        Arrays.fill(dp, -1);
+        return fun(nums,nums.length-1,dp);
     }
-
-    int fun ( int nums[],int i, int dp[]){
-        if ( i>=nums.length)return 0;
-        if (dp[i]!=-1) return dp[i];
-        int a1 = nums[i]+fun(nums,i+2,dp);
-        int a2 = fun ( nums,i+1,dp);
-        return dp[i] = Math.max(a1,a2);
+    static int fun(int[]nums,int ind , int[]dp){
+        if(ind < 0)return 0 ;
+        if(ind == 0 )return nums[0];
+        if(dp[ind] != -1)return dp[ind];
+        int take = nums[ind] + fun(nums,ind-2,dp);
+        int not_take = fun(nums,ind-1,dp);
+        return dp[ind] = Math.max(take,not_take);
     }
 }
