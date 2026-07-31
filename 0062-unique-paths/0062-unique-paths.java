87247ce6b2832bc29363de6b18f2be59d6750c8c
Shadab Alam <shadab29012005@gmail.com>
2026-07-31 11:46:18 +0530

    Time: 0 ms (100%), Space: 42.4 MB (13.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -7,9 +7,9 @@ class Solution {
         return fun(m-1,n-1,dp);
     }
     static int fun(int i , int j,int[][]dp ){
-        if ( i < 0 || j < 0 )return dp[i][j] = 0 ;
+        if ( i < 0 || j < 0 )return  0 ;
         if(dp[i][j]!= -1)return dp[i][j];
-        if ( i == 0 || j == 0 )return dp[i][j] = 1 ;
+        if ( i == 0 && j == 0 )return  1 ;
         
         int up = fun( i-1 , j, dp );
         int left = fun( i , j-1, dp );
