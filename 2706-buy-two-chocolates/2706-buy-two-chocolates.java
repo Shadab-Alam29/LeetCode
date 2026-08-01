c9465d350de7d8ee4c2ac4d5a9c881c3b2d20201
Shadab Alam <shadab29012005@gmail.com>
2026-08-02 01:25:20 +0530

    Time: 3 ms (48.12%), Space: 46.7 MB (10.21%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,9 +1,7 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
         Arrays.sort(prices);
-        for( int i = 0 ; i < prices.length-1 ; i++){
-                if (prices[i]+prices[i+1]<=money)return money-(prices[i]+prices[i+1]) ;
-        }
+        if (prices[0]+prices[1]<=money)return money-(prices[0]+prices[1]);
         return money ;
     }
 }
