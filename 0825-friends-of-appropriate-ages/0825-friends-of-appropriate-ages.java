5599aaac5de723836ac565c1eeec2fb72247df9b
Shadab Alam <shadab29012005@gmail.com>
2026-08-01 18:56:47 +0530

    Time: 3 ms (87.68%), Space: 48 MB (77.59%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
@@ -6,12 +6,12 @@ class Solution {
         }
         int req = 0;
         for (int i = 1; i < users.length; i++) {
-          //  if (users[i] == 0) continue;
+              if (users[i] == 0) continue;
             for (int j = 1; j < users.length; j++) {
                 if (users[j] == 0) continue;
                 if (j <= 0.5 * i + 7) continue;
                 if (j > i) continue;
-
+                if (j > 100 && i < 100 ) continue;
                 req += users[i] * users[j];
                 if (i == j) req -= users[i];
             }
