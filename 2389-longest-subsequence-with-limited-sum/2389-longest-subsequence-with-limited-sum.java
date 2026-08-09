d469ecb62c6838416c884fad3a41e7f8bd85ae80
Shadab Alam <shadab29012005@gmail.com>
2026-08-09 22:31:20 +0530

    Time: 9 ms (58.83%), Space: 46.9 MB (68.88%) - LeetHub

---
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+         Arrays.sort(nums);
+         for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        int ans[] = new int[queries.length];
+          for (int i = 0; i < queries.length; i++) {
+            int start = 0;
+            int end = nums.length - 1;
+            while(start<=end){
+                int mid = start + ( end - start)/2 ;
+                if (nums[mid] <= queries[i]) {
+                    start = mid + 1;
+                }
+                else {
+                     end = mid - 1;
+                } 
+                 ans[i] = start;
+            }
+          }
+          return ans ;
+    }
+}
