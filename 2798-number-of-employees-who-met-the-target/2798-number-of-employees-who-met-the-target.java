370b3035ca2080c7ff17242dd4e3f9efea29e722
Shadab Alam <shadab29012005@gmail.com>
2026-08-14 22:55:48 +0530

    Time: 0 ms (100%), Space: 44.1 MB (60.43%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -2,7 +2,7 @@ class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
         int ans = 0 ; 
         for( int i : hours){
-            if ( target <= i) ans++;
+            if ( i >=target ) ans++;
         }
         return ans ;
     }
