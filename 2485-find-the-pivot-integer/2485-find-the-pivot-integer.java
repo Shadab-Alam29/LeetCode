7ca168a67e37bc1fb68c2d78f025283564b8b998
Shadab Alam <shadab29012005@gmail.com>
2026-08-22 22:53:13 +0530

    Time: 1 ms (71.44%), Space: 42.5 MB (24.65%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,11 +1,14 @@
 class Solution {
     public int pivotInteger(int n) {
-      int total = n * (n + 1) / 2;
-      int left = 0 ; 
-      for ( int i = n ; i >=1 ; i--){
-        left +=i;
-        if (left == total - left + i) return i;
-      }
-      return -1 ;   
+        int total = n * (n + 1) / 2;
+        int left = 0;
+        for (int i = n; i >= 1; i--) {
+            left += i;
+            total -= i;
+            if (total + i == left) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
