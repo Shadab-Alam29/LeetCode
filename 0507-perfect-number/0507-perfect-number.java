ec7e9da90ac8272354b3d6459eb46dd6a7ac8e3e
Shadab Alam <shadab29012005@gmail.com>
2026-08-25 23:43:16 +0530

    Time: 2149 ms (5.1%), Space: 42.1 MB (44.82%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -4,12 +4,9 @@ class Solution {
             return false;
         }
         int sum = 1;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
+        for (int i = 2; i <= num/2; i++) {
             if (num % i == 0) {
                 sum += i;
-                if (i != num / i) {
-                    sum += num / i;
-                }
             }
         }
         return sum == num;
