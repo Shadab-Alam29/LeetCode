c5245cdff55fd94d7026512a06ae8222c6682494
Shadab Alam <shadab29012005@gmail.com>
2026-08-28 17:22:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.4%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int maximum69Number(int num) {
         string s = to_string(num);
-
-        for (int i = 0; i <= s.length()-1; i++) {
+        int x = s.size();
+        for (int i = 0; i <= x-1; i++) {
             if (s[i] == '6') {
                 s[i] = '9';
                 break;
