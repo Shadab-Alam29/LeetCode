d0872294db11814cbc783f0cc93fb76b165f592b
Shadab Alam <shadab29012005@gmail.com>
2026-09-13 01:30:09 +0530

    Time: 2 ms (99.12%), Space: 47.4 MB (25.44%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length() ;
+        int arr[] = new int [n+1];
+        int l = n ; 
+        int d = 0 ;
+        int i = 0 ;
+        for ( char ch : s.toCharArray()){
+           if (ch == 'I')arr[i++] = d++;
+            else arr[i++] = l--;
+        }
+         arr[i] = d;
+        return arr ;
+    }
+}
