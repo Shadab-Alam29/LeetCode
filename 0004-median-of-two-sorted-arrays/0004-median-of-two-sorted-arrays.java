886660cc6949f022e72e04eba2c4fcc580cb53af
Shadab Alam <shadab29012005@gmail.com>
2026-09-12 21:17:00 +0530

    Time: 2 ms (45.95%), Space: 48.6 MB (86.13%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,33 +1,67 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int value:nums1)
-            pq.add(value);
-        for (int value:nums2)
-            pq.add(value);
-        int n = pq.size()-1;
-        if(n%2==0){
-            int i =0;
-            while(  i  != n/2){
-                pq.poll();
-                i++;
-            }
-            return pq.peek();
-        }
-        else{
-            int i = 0 ;
-            double sum  = 0 ;
-            while(i!=n/2){
-                pq.poll();
-                i++;
-            }
-            sum = pq.peek();
-            pq.poll();
-            sum += pq.peek();
-            return sum/2;
 
+        // my method 
+
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // for (int value:nums1)
+        //     pq.add(value);
+        // for (int value:nums2)
+        //     pq.add(value);
+        // int n = pq.size()-1;
+        // if(n%2==0){
+        //     int i =0;
+        //     while(  i  != n/2){
+        //         pq.poll();
+        //         i++;
+        //     }
+        //     return pq.peek();
+        // }
+        // else{
+        //     int i = 0 ;
+        //     double sum  = 0 ;
+        //     while(i!=n/2){
+        //         pq.poll();
+        //         i++;
+        //     }
+        //     sum = pq.peek();
+        //     pq.poll();
+        //     sum += pq.peek();
+        //     return sum/2;
+        //}
+
+        ArrayList<Integer>arr = new ArrayList<>();
+        int a = nums1.length;
+        int b = nums2.length;
+        int i = 0, j = 0, k = 0;
+        while (i < a && j < b) {
+            if (nums1[i] < nums2[j]) {
+                arr.add(nums1[i]);
+                k++;
+                i++;
+            } else {
+                arr.add(nums2[j]) ;
+                k++;
+                j++;
+            }
         }
-        
+         while (i < a) {
+            arr.add(nums1[i]);
+            k++;
+            i++;
+        }        
+        while (j < b) {
+             arr.add(nums2[j]) ;
+             k++;
+             j++;
+        }
+        int n = arr.size();
+        if(n % 2 == 1){
+            return arr.get(n/2);
+        }else{
+           int c =  arr.get(n/2);
+           int d = arr.get(n/2 -1);
+           return (c+d) / 2.0;
+        }
     }
-
 }
